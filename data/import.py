b8ff597648398fa3980d3a04e195aa0b3acabeaf
Fitzgshawn <134566446+Fitzgshawn@users.noreply.github.com>
2023-06-09 21:33:47 -0500

    Update import.py
    Creates request for NBA data; parses JSON, saves to .csv

--- data/import.py
+++ data/import.py
@@ -1,22 +1,38 @@
+import json
+import csv
+from tkinter import W
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
 # Make a GET request to the website
-response = requests.get('https://www.nba.com/stats/players/traditional?SeasonType=Regular+Season')
+url = "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2022-23&SeasonSegment=&SeasonType=Regular%20Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
+headers = { 'Accept': '*/*',
+'Accept-Encoding': 'gzip, deflate, br',
+'Accept-Language': 'en-US,en;q=0.9',
+'Connection': 'keep-alive',
+'Host': 'stats.nba.com',
+'Origin': 'https://www.nba.com',
+'Referer': 'https://www.nba.com/',
+'Sec-Fetch-Dest': 'empty',
+'Sec-Fetch-Mode': 'cors',
+'Sec-Fetch-Site': 'same-site',
+'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
+'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+'sec-ch-ua-mobile': '?1',
+'sec-ch-ua-platform': '"Android"'}
 
-# Parse the HTML response
-soup = BeautifulSoup(response.content, 'html.parser')
+data = requests.get(url, headers=headers).text
 
-# Find the table element
-table = soup.find('table', class_='nba-stat-table')
+nba_object = json.loads(data)
 
-# Extract the data
-data = []
-for row in table.find_all('tr'):
-    cells = row.find_all('td')
-    data.append([cell.text for cell in cells])
+nba_headers = nba_object['resultSets'][0]['headers']
 
-# Save the data to a CSV file
-with open('nba_data.csv', 'w') as f:
+player_data = nba_object['resultSets'][0]['rowSet']
+
+with open('nba_player_list.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
-    writer.writerows(data)
+    writer.writerow(nba_headers)
+    writer.writerows(player_data)
+
+print("done")
