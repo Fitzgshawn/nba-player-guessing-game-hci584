239296eb6b628e79cb97be776bb82f93b0dc3393
Fitzgshawn <134566446+Fitzgshawn@users.noreply.github.com>
2023-06-23 16:17:35 -0500

    Add files via upload

--- main_test_for_v1.py
+++ main_test_for_v1.py
@@ -1,41 +1,6 @@
-import csv
-import random
+from play_again import play_game
+from play_again import get_random_row
 
-def get_random_cell(file_name):
-  """
-  Returns a random cell from the first column of a .csv file.
+play_game()
 
-  Args:
-    file_name: The name of the .csv file.
-
-  Returns:
-    A random cell from the first column of the .csv file.
-  """
-
-  with open(file_name, "r") as f:
-    reader = csv.reader(f)
-    cells = [row[0] for row in reader]
-
-  return random.choice(cells)
-
-
-cell = get_random_cell("data.csv")
-
-
-# Get the user's guess
-guess = input("Guess the NBA player's name: ")
-
-# Check if the user guessed correctly
-if guess == random_player:
-    print("Correct! The player's name is", random_player)
-else:
-    # need to figure out how to give hints
-
-
-# Play again?
-play_again = input("Would you like to play again? (y/n): ")
-
-if play_again == "y":
-    main()
-else:
-    print("Thanks for playing!")
+  
