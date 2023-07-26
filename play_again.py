506732b8dfe691cabcd4f65a4d6a74488e3ec590
Fitzgshawn <134566446+Fitzgshawn@users.noreply.github.com>
2023-07-25 21:25:28 -0500

    Update play_again.py
    added comments

--- play_again.py
+++ play_again.py
@@ -1,6 +1,10 @@
+from itertools import count
+from pydoc import text
 import random
+# import random functionality
 import csv
-def play_again():
+# import csv of nba players and statistics from nba.com
+def play_again(): # define logic to play again after the end of a game
     print("Do you want to play again?")
     comment = input("Yes or No ")
     if comment == "Yes":
@@ -8,65 +12,53 @@ def play_again():
     else: 
         print("thanks for playing!")
 
-# CH I changed the path to the csv file ...
-def get_random_row(csv_file):
+def get_random_row(csv_file): # define random selection of an nba player and where it can be found within repository
     with open("data/" + csv_file, "r") as f:   
         reader = csv.reader(f)
         rows = list(reader)
 
-    return random.choice(rows)
+    return random.choice(rows) # return random nba player
+
+
 def play_game():
     csv_file = "nba_player_list.csv"
 
     row = get_random_row(csv_file)
 
-    # Define the hints  CH: you should line up your comments withing the block structure
+    # Define the hints
     random_player = (row[1])
     ppg = (row[30])
     rpg = (row[22])
     apg = (row[23])
     age = (row[5])
     team = (row[4])
-    # Give the user the points per game 
-    print(f"This player averages {ppg} points per game")
-
-    # CH why not use a loop for this?
  
-    # Make a dict with anything that varies for each hint, here its just one string but 
-    # if you need more just make it a list of strings
-    # Or you could have a dict as dict value instead of a list ...
-    hint_dict = {"rpg": [rpg, "rebounds per game"], "apg":[apg, "assists per game"]}
+    # dict for each hint as strings
+    hint_dict = {"ppg":[ppg, "points per game"],"rpg": [rpg, "rebounds per game"], "apg":[apg, "assists per game"], "age": [age, "years old"], "team": [team, ""]}
 
-    for hint in ["rpg", "apg"]: # could be random ...
+    count = 0 # begin count
+
+    for hint in hint_dict:
+        value, text = list(hint_dict.values())[count]
+        if count == 0: 
+            print(f"This player averages {value} {text}")
 
         # Get the user's guess
         guess = input("Guess the NBA player's name: ")
-
+        
+        count += 1
         if guess == random_player:
             print("Correct! The player's name is", random_player)
             play_again()
-        else:
-            value, text = hint_dict[hint]  # unroll the 2 list elements into 2 variables
+        elif count <3: # if count is less than three, loop back to another hint. either ppg, rpg, or apg
+            value, text = list(hint_dict.values())[count]
             print(f"Incorrect! Your next hint is: This player averages {value} {text}")
-
-    # Check if the user guessed correctly
-    if guess == random_player:
-        print("Correct! The player's name is", random_player)
-        play_again()
-    else:
-        print(f"Incorrect! Your next hint is: This player is {age} years old")
-
-    # Get the user's guess
-    guess = input("Guess the NBA player's name: ")
-
-    # Check if the user guessed correctly
-    if guess == random_player:
-        print("Correct! The player's name is", random_player)
-        play_again()
-    else:
-        print(f"Incorrect! Your final hint is: This player plays for {team}")
-        # Get the user's guess
-    guess = input("Guess the NBA player's name: ")
+        elif count <4:
+            value, text = list(hint_dict.values())[count]
+            print(f"Incorrect! Your next hint is: This player is {value} {text}")
+        elif count <5:
+            value, text = list(hint_dict.values())[count]
+            print(f"Incorrect! Your next hint is: This person plays for {value}")
 
     # Final guess
     if guess == random_player:
